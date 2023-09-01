ae5784863dde80e794ea6d75a326193f3e68569c
roferoserren08@gmail.com <roferoserren08@gmail.com>
2023-09-01 16:34:43 +0800

    ups

--- c/s/d/footer.php
+++ c/s/d/footer.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12">
         <div class="copyright">
-            <p>All rights reserved 2021 | <a href="https://www.sibscontactcenter.com/" style="text-decoration: none;" target="_NEW">SiBS</a> IT Dev Team</p>
+            <p>All rights reserved 2023 | <a href="https://www.sibscontactcenter.com/" style="text-decoration: none;" target="_NEW">SiBS</a> IT Dev Team</p>
         </div>
     </div>
 </div>
